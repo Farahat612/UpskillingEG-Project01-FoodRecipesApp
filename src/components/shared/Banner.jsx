de8dc57d9ded489098dbc570500886bc5156ab90
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-05 15:32:35 +0300

    Banner and Header
    - used them in Dashboard

--- src/components/shared/Banner.jsx
+++ src/components/shared/Banner.jsx
@@ -1,8 +1,9 @@
 import { Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
+import { FaArrowRight } from 'react-icons/fa6'
 import PropTypes from 'prop-types'
 
-const Banner = ({ buttonTitle, buttonDestination, children }) => {
+const Banner = ({ buttonTitle, buttonDestination, children, id }) => {
   const navigate = useNavigate()
   return (
     <div className='d-flex align-items-center home-banner'>
@@ -12,11 +13,11 @@ const Banner = ({ buttonTitle, buttonDestination, children }) => {
         <Button
           variant='success'
           className='btn btn-success px-4 d-flex align-items-center gap-3'
-          id='add-category'
+          id={id}
           onClick={() => navigate(`${buttonDestination}`)}
         >
           {buttonTitle}
-          <i className='fas fa-arrow-right d-inline-block'></i>
+          <FaArrowRight />
         </Button>
       </div>
     </div>
@@ -29,4 +30,5 @@ Banner.propTypes = {
   children: PropTypes.node,
   buttonTitle: PropTypes.string.isRequired,
   buttonDestination: PropTypes.string.isRequired,
+  id: PropTypes.string,
 }
--- src/modules/5.Dashboard/Dashboard.jsx
+++ src/modules/5.Dashboard/Dashboard.jsx
@@ -1,9 +1,28 @@
 import { MasterLayout } from '../../layouts'
+import { Header, Banner } from '../../components/shared'
+import HomeSvg from '../../assets/header/home.svg'
 
 const Dashboard = () => {
   return (
     <MasterLayout>
-      <h1>Welcome to Dashboard</h1>
+      <div className='d-flex flex-column gap-3'>
+        <Header
+          title='Welcome Upskilling!'
+          description='
+          This is a welcoming screen for the entry of the application , you can now see the options
+        '
+          imgUrl={HomeSvg}
+        />
+        <Banner buttonTitle='Fill Recipes' buttonDestination='/recipes'>
+          <h4>
+            Fill the <span className='text-success'>Recipes!</span>
+          </h4>
+          <p>
+            you can now fill the meals easily using the table and form , click
+            here and sill it with the table !
+          </p>
+        </Banner>
+      </div>
     </MasterLayout>
   )
 }
