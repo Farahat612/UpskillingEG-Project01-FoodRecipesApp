37753458d6c8c2f0bfc41e693f20bb7194741a5c
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-09 11:51:17 +0300

    Redundant code : Logic
    - Notify utility

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,30 +1,7 @@
 import { toast } from 'react-toastify'
 
 export const notify = (type, message, time) => {
-  switch (type) {
-    case 'success':
-      toast.success(message, {
-        autoClose: time || 2500,
-      })
-      break
-    case 'error':
-      toast.error(message, {
-        autoClose: time || 2500,
-      })
-      break
-    case 'warning':
-      toast.warning(message, {
-        autoClose: time || 2500,
-      })
-      break
-    case 'info':
-      toast.info(message, {
-        autoClose: time || 2500,
-      })
-      break
-    default:
-      toast(message, {
-        autoClose: time || 2500,
-      })
-  }
+  toast[type](message, {
+    autoClose: time || 2500,
+  })
 }
