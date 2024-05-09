db43fa00ef8f7459293604a26487317a1acecd61
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-09 14:40:34 +0300

    Axios Auth issue
    - update axios configuration to use interceptor for setting auth token before each request to solve the missing token after signout signin

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseURL = 'https://upskilling-egypt.com:3006/api/v1'
 const staticURL = 'https://upskilling-egypt.com:3006'
-const token = `Bearer ${localStorage.getItem('token')}`
+
 
 // axios instance without token
 const apiPublic = axios.create({
@@ -15,10 +15,12 @@ const apiPublic = axios.create({
 // axios instance with token
 const apiProtected = axios.create({
   baseURL,
-  headers: {
-    // 'Content-Type': 'application/json',
-    Authorization: token,
-  },
+})
+apiProtected.interceptors.request.use((config) => {
+  const token = `Bearer ${localStorage.getItem('token')}`
+  config.headers.Authorization = token
+
+  return config
 })
 
 export { apiPublic, apiProtected, baseURL, staticURL }
