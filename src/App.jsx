04139805d03c334d887eb0fd9ceca6053df993e4
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-10 22:18:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ function App() {
             <Route path='resetPass' element={<ResetPass />} />
           </Route>
 
-          {/* <Route path='*' element={<NotFound />} /> */}
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
     </>
